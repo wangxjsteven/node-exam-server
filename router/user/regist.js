245f3630b941208ccefed148eae7b774979bf5d6
王雪进 <wangxuejin@wangxuejindeMacBook-Pro.local>
2018-03-20 21:05:04 +0800

    [exam 注册] bug修复

--- router/user/regist.js
+++ router/user/regist.js
@@ -1,4 +1,4 @@
-let {User} = require('../../model/user.js')
+let { User } = require('../../model/user.js')
 
 /**
  * @author: kelly
@@ -7,19 +7,27 @@ let {User} = require('../../model/user.js')
  * @description: 登出
  */
 let regist = function(req, res, next) {
-    
-    // console.log('user:', User)
-    // console.log(req)
-    if (!User.find({ username: /admin/ })) {
-
-        let user1 = new User({ username: 'admin', password: 123456 })
-        user1.save(function(err, doc) {
-            console.log(doc);
-        })
-        return res.send(PayloadSuccess())
-    }else{
-        return res.send(PayloadException('BUSINESS_ERROR','已有同名账户，无法重复注册，请知悉！'))
-    }
+    let { username, password,phone } = req.query || {},
+        reg = new RegExp(username)
+    console.log(username,reg)
+    User.find({ username: reg }, (err, list) => {
+        if (err) {
+            return res.send(PayloadException('BUSINESS_ERROR', '获取admin失败，原因：' + err))
+        }
+        if (!list || list.length === 0) {
+            let user1 = new User({
+                username: username,
+                password: password,
+                phone:phone||''
+            })
+            user1.save(function(err, doc) {
+                console.log(doc);
+            })
+            res.send(PayloadSuccess())
+        } else {
+            res.send(PayloadException('BUSINESS_ERROR', '已有同名账户，无法重复注册，请知悉！'))
+        }
+    })
 };
 
 module.exports = regist
